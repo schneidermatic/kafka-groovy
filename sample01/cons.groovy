1679d05cc06cf4071bfd19d4d81f8ac882d72735
schneidermatic <markus.schneider73@gmail.com>
2018-08-07 13:26:56 +0200

    The following files were initially committed.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   sample01/cons.groovy
    	new file:   sample01/prod.groovy

---
+++ sample01/cons.groovy
@@ -0,0 +1,44 @@
+#!/bin/usr/env groovy
+
+@Grab(group='org.apache.kafka', module='kafka-clients', version='2.0.0')
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
+class App {
+   
+   static void main(args) throws Exception {
+
+      //Kafka consumer configuration settings
+      def topicName = "sample"
+
+      if(! args.length == 0){
+         println("Set topicName: $topicName");
+      }     
+      
+      def consumer = new KafkaConsumer(
+                  [ 
+                   'bootstrap.servers': 'localhost:9092',
+                   'group.id': "test",
+                   'enable.auto.commit': "true",
+                   'auto.commit.interval.ms': "1000",
+                   'session.timeout.ms': "30000",
+                   'key.deserializer':"org.apache.kafka.common.serialization.StringDeserializer",
+                   'value.deserializer':"org.apache.kafka.common.serialization.StringDeserializer"
+                  ]
+      )
+      
+      //Kafka Consumer subscribes list of topics here.
+      consumer.subscribe([topicName])
+      
+      //print the topic name
+      println("Subscribed to topic " + topicName)
+      
+      while (true) {
+         for (record in consumer.poll(100))
+         // print the offset,key and value for the consumer records.
+         printf("offset = %d, key = %s, value = %s\n",record.offset(), record.key(), record.value())
+      }
+   }
+}
---
+++ sample01/prod.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+
+@Grab(group='org.apache.kafka', module='kafka-clients', version='2.0.0')
+
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+
+class App {
+   
+   static void main(args) throws Exception{
+
+      //Assign topicName to string variable
+      def topicName = "sample"
+      
+      if(! args.length == 0){
+         println("Set topicName: $topicName")
+      }
+
+      def producer = new KafkaProducer( 
+        [      
+               'bootstrap.servers':'localhost:9092',
+               'acks':'all',
+               'retries':0,
+               'batch.size': 16384,
+               'linger.ms': 1,
+               'buffer.memory': 33554432,
+               'key.serializer': "org.apache.kafka.common.serialization.StringSerializer",
+               'value.serializer': "org.apache.kafka.common.serialization.StringSerializer"
+        ]
+      )
+
+      for(i in 0..9)
+         producer.send(new ProducerRecord<String, String>(topicName,Integer.toString(i), Integer.toString(i)))
+         println("Message sent successfully")
+         producer.close()
+   }
+}
